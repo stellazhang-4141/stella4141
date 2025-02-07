23ce3150643d4cee6a999c335cc83c70db8c252e
stellazhang-4141 <stella4747zhang4141@gmail.com>
2025-02-07 14:00:34 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-#api cannnot appear in cloud
-
 from flask import Flask
 from flask import render_template,request
 import textblob
+import os
+import google.generativeai as genai
 
-app = Flask("__name__")
+api = os.getenv("makersuite")
+genai.configure(api_key=api)
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -17,15 +21,27 @@ def main():
 
 @app.route("/SA",methods=["GET","POST"])
 def SA():
-    name=request.form.get("q")
     return(render_template("SA.html"))
 
 @app.route("/SA_result",methods=["GET","POST"])
 def SA_result():
     q = request.form.get("q")
-    r = textblob.Textblob(q).sentiment
+    r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
+
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    return(render_template("genAI.html"))
+
+@app.route("/genAI_result",methods=["GET","POST"])
+def genAI_result():
+    q = request.form.get("q")
+    r = model.generate_content(q)
+    return(render_template("genAI_result.html",r=r.candidates[0].content.parts[0].text))
+
+@app.route("/paynow",methods=["GET","POST"])
+def paynow():
+    return(render_template("paynow.html"))
 
 if __name__ == "__main__":
     app.run()
-
